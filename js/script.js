7f5d812fe3b0cdc11bc6bfa4a6646256fcee1f9b
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2024-11-07 21:27:07 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,10 +20,10 @@ function menuShow() {
   let menuMobile = document.querySelector(".mobile-menu");
   if (menuMobile.classList.contains("open")) {
     menuMobile.classList.remove("open");
-    document.querySelector(".icon").src = "../public/img/menu_white_36dp.svg";
+    document.querySelector(".icon").src = "./public/img/menu_white_36dp.svg";
   } else {
     menuMobile.classList.add("open");
-    document.querySelector(".icon").src = "../public/img/close_white_36dp.svg";
+    document.querySelector(".icon").src = "./public/img/close_white_36dp.svg";
   }
 }
 
